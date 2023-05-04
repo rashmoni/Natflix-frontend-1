cb152cb561ac9f5f5f35bbd26aac407eece08843
Eduardo <el_alienx@hotmail.com>
2023-05-04 16:11:17 +0200

    ready to merge

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -48,13 +48,24 @@ export default function Login() {
         <h1>Sign In</h1>
         <form onSubmit={(event) => onSubmit(event)}>
           <ListInput fields={Fields} state={[form, setForm]} />
-          <button>Sign in</button>
+          <button className="primary">Sign in</button>
         </form>
         <footer>
           <p>
-            New to Natflix? <Link to="/registration">Sign up now</Link>.
+            New to Natflix? <Link to="/registration">Sign up now</Link>
           </p>
         </footer>
+      </div>
+      <div className="warning">
+        <h2>⚠️ Note:</h2>
+        <p>
+          To login as a <b>Customer</b> use <code>customer@gmail.com</code> with
+          password <code>customer</code>
+        </p>
+        <p>
+          To login as an <b>Admin</b> use <code>admin@gmail.com</code> with
+          password <code>admin</code>
+        </p>
       </div>
     </div>
   );
